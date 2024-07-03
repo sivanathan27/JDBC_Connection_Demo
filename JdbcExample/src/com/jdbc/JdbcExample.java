1edca4c5cd85088a839a744a1562638d903cfe26
sivanathan27 <128278742+sivanathan27@users.noreply.github.com>
2024-07-03 14:47:13 +0530

    Update JdbcExample.java

--- JdbcExample/src/com/jdbc/JdbcExample.java
+++ JdbcExample/src/com/jdbc/JdbcExample.java
@@ -7,6 +7,7 @@ import java.sql.Statement;
 
 public class JdbcExample {
     public static void main(String[] args) {
+        
         // Database URL
         String jdbcUrl = "jdbc:mysql://localhost:3306/testdb";
         // Database credentials
